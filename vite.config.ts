98917397a4c93dd6bed6be59c6546c859e2a5e7d
sebasruiz09 <sebasruiz909@gmail.com>
2023-02-27 22:07:56 -0500

    CONFIG : change output directory and set deploy config gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base : './',
-  plugins: [react()],
-  resolve : {
-    alias : {
-      '@Portfolio' : path.resolve(__dirname, 'src'),
+    base: "./Portfolio_v2",
+    plugins: [react()],
+    build: {
+        outDir: "./build",
     },
-  },
-  server :{
-    port : 5000,
-    strictPort : true,
-  }
-
-})
+    resolve: {
+        alias: {
+            "@Portfolio": path.resolve(__dirname, "src"),
+        },
+    },
+    server: {
+        port: 5000,
+        strictPort: true,
+    },
+});
